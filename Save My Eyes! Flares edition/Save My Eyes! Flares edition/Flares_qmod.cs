c4c131f13bbfcd302a661004277b0e22955a0b32
Rose Cummins <kelsarhu@gmail.com>
2021-01-03 18:36:57 -0700

    Flares light intensity Mod

---
+++ Save My Eyes! Flares edition/Save My Eyes! Flares edition/Flares_qmod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+
+namespace Save_My_Eyes__Flares_edition
+{
+    public class Flares_qmod
+    {
+        public static void Patch()
+        {
+            {
+                var harmony = HarmonyInstance.Create("com.rose.subnautica.Save_My_Eyes__Flares_edition");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+        }
+    }
+}
---
+++ Save My Eyes! Flares edition/Save My Eyes! Flares edition/flare_intensity_Patch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+
+namespace Save_My_Eyes__Flares_edition
+{
+    [HarmonyPatch(typeof(Flare))]
+    [HarmonyPatch("UpdateLight")]
+
+    class Flare_intensity_Patch
+    {
+        [HarmonyPostfix]
+        public static void  Postfix(Flare __instance)
+        {
+            __instance.light.intensity = Mathf.Lerp(.1f, 1.5f, 15f);
+        }
+    }
+}
